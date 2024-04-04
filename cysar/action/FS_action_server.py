69137e74d0927227f48a6bbe9d9af4023fd41984
ecabelin <ecabelin@Ubuntu202.myguest.virtualbox.org>
2024-04-03 19:54:24 -0500

    Error with goal_handle conversion for time_length

--- cysar/action/FS_action_server.py
+++ cysar/action/FS_action_server.py
@@ -39,7 +39,7 @@ class FoursquareActionServer(Node):
         since we don't want to start an autonoumous program, and the terrain causes the robot to move not as expected] 
         '''
         # Run an action execution and output new joystick values from terminal.
-        time_length = time.time + goal_handle
+        time_length = time.time + float(goal_handle)
 
         while time.time() < time_length:
             self.joystick.stick_left_y = 1
